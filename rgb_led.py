71e95897b258e1d707f920662af7d27ab7d959d4
Jeremy Poulter <jeremy@bigjungle.net>
2018-12-20 21:31:58 +0000

    Added setting the colour manually

--- rgb_led.py
+++ rgb_led.py
@@ -3,6 +3,7 @@ import board
 import neopixel
 import adafruit_irremote
 import pulseio
+import time
 
 # Set up the 10 Circuit Playground Express NeoPixels half bright
 #num_pixels = 10
@@ -12,16 +13,42 @@ import pulseio
 pixels = neopixel.NeoPixel(board.NEOPIXEL, 10, brightness=.2)
 
 savedColour = 0
+mode = 0
 
 def fill(colour):
   pixels.fill(colour)
   global savedColour
   savedColour = colour
 
+def setRed(red):
+  green = savedColour[1]
+  blue = savedColour[2]
+  fill((red, green, blue))
+
+def setGreen(green):
+  red = savedColour[0]
+  blue = savedColour[2]
+  fill((red, green, blue))
+
+def setBlue(blue):
+  red = savedColour[0]
+  green = savedColour[1]
+  fill((red, green, blue))
+
 def off():
   pixels.fill((0, 0, 0))
 
 def on():
+  pixels.fill(savedColour)
+
+def setMode():
+  global mode
+  mode = mode + 1
+  if mode > 1:
+    mode = 0
+  pixels.fill((0,0,0))
+  pixels[mode] = (0,0,255)
+  time.sleep(0.3)
   pixels.fill(savedColour)
 
 def brightness(brightness):
@@ -38,23 +65,23 @@ command_table = [
   { "code": [255, 0, 135, 120], "action": lambda : off() },
   { "code": [255, 0, 7, 248],   "action": lambda : on() },
 
-  { "code": [255, 0, 231, 24],  "action": lambda : fill((255, 0, 0)) },
-  { "code": [255, 0, 223, 32],  "action": lambda : fill((255, 40, 0)) },
-  { "code": [255, 0, 239, 16],  "action": lambda : fill((255, 80, 0)) },
-  { "code": [255, 0, 207, 48],  "action": lambda : fill((255, 120, 0)) },
-  { "code": [255, 0, 247, 8],   "action": lambda : fill((255, 160, 0)) },
+  { "code": [255, 0, 231, 24],  "action": lambda : fill((255, 0, 0)) if mode == 0 else setRed(252) },
+  { "code": [255, 0, 223, 32],  "action": lambda : fill((255, 40, 0)) if mode == 0 else setRed(189) },
+  { "code": [255, 0, 239, 16],  "action": lambda : fill((255, 80, 0)) if mode == 0 else setRed(126) },
+  { "code": [255, 0, 207, 48],  "action": lambda : fill((255, 120, 0)) if mode == 0 else setRed(63) },
+  { "code": [255, 0, 247, 8],   "action": lambda : fill((255, 160, 0)) if mode == 0 else setRed(0) },
   
-  { "code": [255, 0, 103, 152], "action": lambda : fill((0, 255, 0)) },
-  { "code": [255, 0, 95, 160],  "action": lambda : fill((0, 200, 50)) },
-  { "code": [255, 0, 111, 144], "action": lambda : fill((0, 150, 100)) },
-  { "code": [255, 0, 79, 176],  "action": lambda : fill((0, 100, 150)) },
-  { "code": [255, 0, 119, 136], "action": lambda : fill((0, 50, 200)) },
+  { "code": [255, 0, 103, 152], "action": lambda : fill((0, 255, 0)) if mode == 0 else setGreen(252) },
+  { "code": [255, 0, 95, 160],  "action": lambda : fill((0, 200, 50)) if mode == 0 else setGreen(189) },
+  { "code": [255, 0, 111, 144], "action": lambda : fill((0, 150, 100)) if mode == 0 else setGreen(126) },
+  { "code": [255, 0, 79, 176],  "action": lambda : fill((0, 100, 150)) if mode == 0 else setGreen(63) },
+  { "code": [255, 0, 119, 136], "action": lambda : fill((0, 50, 200)) if mode == 0 else setGreen(0) },
 
-  { "code": [255, 0, 167, 88],  "action": lambda : fill((0, 0, 255)) },
-  { "code": [255, 0, 159, 96],  "action": lambda : fill((40, 0, 255)) },
-  { "code": [255, 0, 175, 80],  "action": lambda : fill((80, 0, 255)) },
-  { "code": [255, 0, 143, 112], "action": lambda : fill((120, 0, 250)) },
-  { "code": [255, 0, 183, 72],  "action": lambda : fill((250, 0, 120)) },
+  { "code": [255, 0, 167, 88],  "action": lambda : fill((0, 0, 255)) if mode == 0 else setBlue(252) },
+  { "code": [255, 0, 159, 96],  "action": lambda : fill((40, 0, 255)) if mode == 0 else setBlue(189) },
+  { "code": [255, 0, 175, 80],  "action": lambda : fill((80, 0, 255)) if mode == 0 else setBlue(126) },
+  { "code": [255, 0, 143, 112], "action": lambda : fill((120, 0, 250)) if mode == 0 else setBlue(63) },
+  { "code": [255, 0, 183, 72],  "action": lambda : fill((250, 0, 120)) if mode == 0 else setBlue(0) },
 
   { "code": [255, 0, 215, 40],  "action": lambda : fill((255, 255, 255)) },
 
@@ -63,6 +90,8 @@ command_table = [
   { "code": [255, 0, 47, 208],  "action": lambda : brightness(0.6) },
   { "code": [255, 0, 15, 240],  "action": lambda : brightness(0.4) },
   { "code": [255, 0, 55, 200],  "action": lambda : brightness(0.2) },
+
+  { "code": [255, 0, 23, 232],  "action": lambda : setMode() },
 ]
 
 while True:
