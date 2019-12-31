be813dc5c1bf61987826f0667fff85c4a4428158
Jeremy Poulter <jeremy@bigjungle.net>
2019-12-31 15:42:26 +0000

    Working version

--- lpf_send.py
+++ lpf_send.py
@@ -1,20 +1,22 @@
-from adafruit_circuitplayground.express import cpx
+#from adafruit_circuitplayground.express import cpx
 import board
-import time
 import adafruit_irremote
 import pulseio
+from digitalio import DigitalInOut, Direction, Pull
 
-# Set up the 10 Circuit Playground Express NeoPixels half bright
+import neopixel
+
+pixel_pin = board.NEOPIXEL
 num_pixels = 10
-pixels = cpx.pixels
-cpx.pixels.brightnes = 0.1
- 
-# Create a 'pulseio' output, to send infrared signals on the IR transmitter @ 38KHz
+
+pixels = neopixel.NeoPixel(pixel_pin, num_pixels, brightness=0.3, auto_write=False)
+
+## Create a 'pulseio' output, to send infrared signals on the IR transmitter @ 38KHz
 pwm = pulseio.PWMOut(board.IR_TX, frequency=38000, duty_cycle=2 ** 15)
 pulseout = pulseio.PulseOut(pwm)
 # Create an encoder that will take numbers and turn them into NEC IR pulses
-encoder = adafruit_irremote.GenericTransmit(header=[9500, 4500], one=[550, 550],
-                                            zero=[550, 1700], trail=0)
+encoder = adafruit_irremote.GenericTransmit(header=[156, 1014], one=[156, 546],
+                                            zero=[156, 260], trail=0)
  
 channel = 0
 address = 0
@@ -33,41 +35,65 @@ BLUE = (0, 0, 255)
 PURPLE = (180, 0, 255)
 BLACK = (0, 0, 0)
 
+button_a = DigitalInOut(board.BUTTON_A)
+button_a.direction = Direction.INPUT
+button_a.pull = Pull.DOWN
+
+button_a_last = False
+
+button_b = DigitalInOut(board.BUTTON_B)
+button_b.direction = Direction.INPUT
+button_b.pull = Pull.DOWN
+
+button_b_last = False
+
 def setPwm(pwm):
+    global toggle
     print("Set pwm %d" % pwm)
 
-    nibble1 = (toggle << 3) & channel 
-    nibble2 = (address << 3) & (1 << 2) & output
+    print("toggle = %d, channel = %d, output = %d, address = %d" % (toggle, channel, output, address))
+
+    nibble1 = (toggle << 3) | channel 
+    nibble2 = (address << 3) | (1 << 2) | output
 
     if 0 == pwm:
         nibble3 = 1 << 3
     elif pwm < 0:
-        nibble3 = (1 << 3) & (8 + pwm)
+        nibble3 = (1 << 3) | (8 + pwm)
     else:
         nibble3 = pwm
 
     nibble4 = 0xF ^ nibble1 ^ nibble2 ^ nibble3
 
-    byte1 = (nibble1 << 4) & nibble2
-    byte2 = (nibble3 << 4) & nibble4
+    print("Nibbles: ", nibble1, nibble2, nibble3, nibble4)
+
+    byte1 = (nibble1 << 4) | nibble2
+    byte2 = (nibble3 << 4) | nibble4
 
     colour = GREEN if pwm >= 0 else BLUE
-    pwm = pwm if pwm >= 0 else -pwm
-    print (pwm, colour)
+    pwm = abs(pwm)
+    #print (pwm, colour)
 
     for i in range(num_pixels):
         pixels[i] = colour if i < pwm else BLACK
     
     pixels.show()
 
-    encoder.transmit(pulseout, [~byte1, ~byte2])
+    print("Sending code: %s %s" % ('{:08b}'.format(byte1), '{:08b}'.format(byte2)))
+    encoder.transmit(pulseout, [byte1, byte2])
+
+    toggle = (toggle + 1) % 2
 
 while True:
-    if cpx.button_a:
-        if pwm < MAX_PWM:
-            pwm = pwm + 1
-            setPwm(pwm)
-    if cpx.button_b:
-        if pwm > MIN_PWM:
-            pwm = pwm - 1
-            setPwm(pwm)
+    if button_a_last != button_a.value:
+        button_a_last = button_a.value
+        if button_a.value:
+            if pwm < MAX_PWM:
+                pwm = pwm + 1
+                setPwm(pwm)
+    if button_b_last != button_b.value:
+        button_b_last = button_b.value
+        if button_b.value:
+            if pwm > MIN_PWM:
+                pwm = pwm - 1
+                setPwm(pwm)
